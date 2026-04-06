621c38e4b4b29a4cc7dab6f1f7fc0e04b8b184dd
sezerkara00 <71710799+sezerkara00@users.noreply.github.com>
2026-04-06 16:27:47 +0300

    upt

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true, // Bunu ekle: Sayfa yollarının sonuna "/" ekler ve çakışmayı önler
   images: {
     unoptimized: true,
   },
